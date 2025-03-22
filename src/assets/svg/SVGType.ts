a70e2835a42440b053437fde03a2fedb35d32c2b
Parth Modi <parthmodi.developer@gmail.com>
2025-03-22 12:38:17 +0530

    fix: resolved error

---
+++ src/assets/svg/SVGType.ts
@@ -0,0 +1,9 @@
+export type ISVGType = {
+  color?: string;
+  height?: number;
+  width?: number;
+  stroke?: string;
+  innerColor?: string;
+  className?: string;
+  active?: boolean;
+}
---
+++ src/assets/svg/index.tsx
@@ -0,0 +1,5 @@
+import { default as CloseIconSVG } from './CloseIconSVG';
+
+export {
+  CloseIconSVG,
+};
