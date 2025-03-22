b0f999c55b0310b180f86427bfe17d54ca525d50
Parth Modi <parthmodi.developer@gmail.com>
2025-03-22 16:32:44 +0530

    align component on trip information

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,11 +2,7 @@ import TripInformation from '../TripInformation';
 import './Dashboard.scss';
 
 const Dashboard = () => {
-  const busTypes = ["Standard", "Luxury", "Mini", "Double Decker"];
 
-  const handleSelection = (selectedValue: string) => {
-    console.log("Selected Bus Type:", selectedValue);
-  };
   return (
     <div className="main-dashboard-container">
       <TripInformation />
