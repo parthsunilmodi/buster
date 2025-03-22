0fae452e74f54f1a6396f246eb1a4bbcd91173ed
Parth Modi <parthmodi.developer@gmail.com>
2025-03-22 16:12:37 +0530

    feat : custom file selector

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,19 @@
+import CustomDropdown from '../../components/CustomDropdown';
+import FileSelector from '../../components/FileSelector';
 import './Dashboard.scss';
 
 const Dashboard = () => {
+  const busTypes = ["Standard", "Luxury", "Mini", "Double Decker"];
+
+  const handleSelection = (selectedValue: string) => {
+    console.log("Selected Bus Type:", selectedValue);
+  };
   return (
+    <>
     <div className="main-dashboard-container">Dashboard</div>
+    <FileSelector/>
+    <CustomDropdown label="Preferred bus type:" options={busTypes} onSelect={handleSelection} />
+    </>
   )
 }
 
