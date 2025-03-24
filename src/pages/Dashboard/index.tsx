bc9c4f183975c143913ebfb25568757407d88787
Parth Modi <parthmodi.developer@gmail.com>
2025-03-24 16:11:17 +0530

    feat: top banner

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import PricingIcon from '../../assets/images/visualelectric.png';
 import TripInformationForm from '../TripInformationForm';
 import ContactInformationForm from '../ContactInformationForm';
 import './Dashboard.scss';
@@ -8,16 +7,6 @@ const Dashboard = () => {
 
   return (
     <div className="main-dashboard-container">
-      <div className="clear-pricing">
-        <div className="details-wrapper">
-          <h1>Clear pricing in 1 day or less</h1>
-          <p className="sub-heading">For simple trips, our price quote is <span>instant.</span></p>
-        </div>
-        <div className="pricing-img-wrapper">
-          <img src={PricingIcon} alt="pricing" />
-          <p className="header-card">Here every step to get you there comfortably & safely.</p>
-        </div>
-      </div>
       <TripInformationForm />
       <ContactInformationForm />
     </div>
---
+++ src/pages/TopBanner/index.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import PricingIcon from '../../assets/images/visualelectric.png';
+import './TopBanner.scss';
+
+const TopBanner = () => {
+
+  return (
+    <div className="top-banner container">
+      <div className="clear-pricing">
+        <div className="details-wrapper">
+          <h1>Clear pricing in 1 day or less</h1>
+          <p className="sub-heading">For simple trips, our price quote is <span>instant.</span></p>
+        </div>
+        <div className="pricing-img-wrapper">
+          <img src={PricingIcon} alt="pricing" />
+          <p className="header-card">Here every step to get you there comfortably & safely.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopBanner;
