9a2b48b87900b0bd15524184cd0ec7de5e58e5e6
Parth Modi <parthmodi.developer@gmail.com>
2025-03-24 13:59:15 +0530

    feat: changes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -8,16 +8,16 @@ const Dashboard = () => {
 
   return (
     <div className="main-dashboard-container">
-      {/* <div className="clear-pricing">
-          <div className="details-wrapper">
-            <h1>Clear pricing in 1 day or less</h1>
-            <p className="sub-heading">For simple trips, our price quote is <span>instant.</span></p>
-          </div>
-          <div className="pricing-img-wrapper">
-            <img src={PricingIcon} alt="pricing" />
-            <p className="header-card">Here every step to get you there comfortably & safely.</p>
-          </div>
-        </div> */}
+      <div className="clear-pricing">
+        <div className="details-wrapper">
+          <h1>Clear pricing in 1 day or less</h1>
+          <p className="sub-heading">For simple trips, our price quote is <span>instant.</span></p>
+        </div>
+        <div className="pricing-img-wrapper">
+          <img src={PricingIcon} alt="pricing" />
+          <p className="header-card">Here every step to get you there comfortably & safely.</p>
+        </div>
+      </div>
       <TripInformationForm />
       <ContactInformationForm />
     </div>
