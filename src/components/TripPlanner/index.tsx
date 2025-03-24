3a381d972c405640038ec2f19e173b7db31943a4
Parth Modi <parthmodi.developer@gmail.com>
2025-03-24 12:27:40 +0530

    feat: impmelent drag and drop

--- src/components/TripPlanner/index.tsx
+++ src/components/TripPlanner/index.tsx
@@ -1,68 +1,111 @@
-import { useState } from 'react';
+import React, { useState } from "react";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import DatePicker from 'react-datepicker';
+import ClockIcon from '../../assets/images/clock-icon.png';
+import CalenderIcon from '../../assets/images/clock-icon.png';
 import InputField from '../Input/Input';
-import plusIcon from '../../assets/images/plusIcon.png'
-import './TripPlanner.scss';
-import CustomDateRange from '../DateRangePicker/index';
+const ItemType = "DRAGGABLE_ITEM";
 
-const TripPlanner = () => {
-  const stopsList = [
-    { location: '', date: '', time: '' },
-    { location: '', date: '', time: '' },
-  ];
-  const [stops, setStops] = useState(stopsList);
-  const [trip, setTrip] = useState({ start: '', destination: '', date: '', time: '' });
+const DraggableItem = ({ item, index, moveItem }) => {
+  const [, ref] = useDrag({
+    type: ItemType,
+    item: { index },
+  });
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [startTime, setStartTime] = useState<Date | null>(null);
 
-  const addStop = () => {
-    setStops([...stops, { location: '', date: '', time: '' }]);
-  };
+  const [, drop] = useDrop({
+    accept: ItemType,
+    hover: (draggedItem) => {
+      if (draggedItem.index !== index) {
+        moveItem(draggedItem.index, index);
+        draggedItem.index = index;
+      }
+    },
+  });
 
   return (
-    <div className="">
-      <div className="trip-planner">
-        <div className="stepper-container">
-          <div className="stepper-line"></div>
-          <div className="stepper-item start"></div>
-          {stops.map((_, index) => (
-            <div key={index} className="stepper-item stop">....</div>
-          ))}
-          <div className="stepper-item end"></div>
-        </div>
-
-        <div>
-          <div className="single-location">
-            <div>
-              <InputField type="text" label="Starting from *"/>
-            </div>
-            <div>
-              <div>On *</div>
-              <CustomDateRange />
-            </div>
-            <div>
-              <div>At</div>
-              <input type="time"/>
-            </div>
-          </div>
-          <button className="common-btn" onClick={addStop}> <img src={plusIcon} alt="PlusIcon" /> Add a stop</button>
-
-          {stops.map((stop, index) => (
-            <div className="mb-3" key={index}>
-              <div>
-                <input type="text" placeholder="Stop location"/>
-              </div>
-              <div>
-                <input type="date"/>
-              </div>
-              <div>
-                <input type="time"/>
-              </div>
-            </div>
-          ))}
-
-          <button variant="outline-primary" onClick={addStop}>+ Add a stop</button>
-        </div>
+    <div className="d-flex flex-row items-center gap-4 p-4" ref={(node) => ref(drop(node))}>
+      {/* Starting From Input */}
+      <div className="w-50">
+        <InputField value={item.text} label="Starting From*"  />
       </div>
+      <div className="w-25">
+        <InputField value={"disldj"} label="Starting From*" />
+      </div>
+      <div className="w-25">
+        <InputField value="hduksh" label="Starting From*" />
+      </div>
+
+      {/*/!* Date Picker *!/*/}
+      {/*<div className="flex flex-col">*/}
+      {/*  <label className="text-sm font-medium">*/}
+      {/*    On <span className="text-red-500">*</span>*/}
+      {/*  </label>*/}
+      {/*  <div className="relative">*/}
+      {/*    <DatePicker*/}
+      {/*      selected={startDate}*/}
+      {/*      onChange={(date) => setStartDate(date)}*/}
+      {/*      className="w-32 p-2 border rounded-md bg-gray-100"*/}
+      {/*      placeholderText="Select date"*/}
+      {/*    />*/}
+      {/*    <img src={CalenderIcon} alt="calender" />*/}
+      {/*    /!*<CalendarIcon className="w-5 h-5 absolute right-2 top-3 text-gray-500" />*!/*/}
+      {/*  </div>*/}
+      {/*</div>*/}
+
+      {/*/!* Time Picker *!/*/}
+      {/*<div className="flex flex-col">*/}
+      {/*  <label className="text-sm font-medium">At</label>*/}
+      {/*  <div className="relative">*/}
+      {/*    <DatePicker*/}
+      {/*      selected={startTime}*/}
+      {/*      onChange={(time) => setStartTime(time)}*/}
+      {/*      showTimeSelect*/}
+      {/*      showTimeSelectOnly*/}
+      {/*      timeIntervals={15}*/}
+      {/*      timeCaption="Time"*/}
+      {/*      dateFormat="h:mm aa"*/}
+      {/*      className="w-32 p-2 border rounded-md bg-gray-100"*/}
+      {/*      placeholderText="Select time"*/}
+      {/*    />*/}
+      {/*    <img src={ClockIcon} alt="calender" />*/}
+      {/*    /!*<ClockIcon className="w-5 h-5 absolute right-2 top-3 text-gray-500" />*!/*/}
+      {/*  </div>*/}
+      {/*</div>*/}
     </div>
   );
 };
 
-export default TripPlanner;
+const DraggableList = () => {
+  const [items, setItems] = useState([
+    { id: 1, text: "Harsh Common Service Center, Shop No-" },
+    { id: 2, text: "xzCs" },
+    { id: 3, text: "Another Item" },
+  ]);
+
+  const moveItem = (fromIndex, toIndex) => {
+    const updatedItems = [...items];
+    const [movedItem] = updatedItems.splice(fromIndex, 1);
+    updatedItems.splice(toIndex, 0, movedItem);
+    setItems(updatedItems);
+  };
+
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div className="w-100 border p-4">
+        {items.map((item, index) => (
+          <DraggableItem
+            key={item.id}
+            item={item}
+            index={index}
+            moveItem={moveItem}
+          />
+        ))}
+      </div>
+    </DndProvider>
+  );
+};
+
+export default DraggableList;
