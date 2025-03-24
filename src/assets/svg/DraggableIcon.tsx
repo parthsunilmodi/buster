1a2e8c4910e4f500455898cd1e9cc1f119efb4af
Parth Modi <parthmodi.developer@gmail.com>
2025-03-24 19:07:57 +0530

    feat: drag and drop and delete functionality

---
+++ src/assets/svg/DraggableIcon.tsx
@@ -0,0 +1,20 @@
+import { ISVGType } from './SVGType';
+
+const DraggableIcon = (props: ISVGType) => {
+  const { width = 30, height = 30, color = "#0A0C30" } = props;
+  return (
+    <svg width="16" height="26" viewBox="0 0 16 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="15" height="25" transform="translate(0.759766 0.5)" fill="white"/>
+      <ellipse cx="3.09175" cy="2.78076" rx="2.33199" ry="2.28076" fill="#6C6C6C"/>
+      <ellipse cx="3.09175" cy="12.7808" rx="2.33199" ry="2.28076" fill="#6C6C6C"/>
+      <ellipse cx="3.09175" cy="22.7808" rx="2.33199" ry="2.28076" fill="#6C6C6C"/>
+      <ellipse cx="13.0918" cy="2.78076" rx="2.33199" ry="2.28076" fill="#6C6C6C"/>
+      <ellipse cx="13.0918" cy="12.7808" rx="2.33199" ry="2.28076" fill="#6C6C6C"/>
+      <ellipse cx="13.0918" cy="22.7808" rx="2.33199" ry="2.28076" fill="#6C6C6C"/>
+    </svg>
+
+
+  );
+};
+
+export default DraggableIcon;
--- src/pages/TripInformationForm/TripCards.tsx
+++ src/pages/TripInformationForm/TripCards.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React  from 'react';
 import CustomCard from '../../components/CustomCard';
 import { data } from '../../constants';
 import './TripInformationForm.scss';
