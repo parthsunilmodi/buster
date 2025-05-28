30ec4f8491b2d2143131558065d66cf72a083b75
Parth Modi <parthmodi.developer@gmail.com>
2025-05-28 18:05:00 +0530

    fix: build warnings and errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import 'react-datepicker/dist/react-datepicker.css';
-import App from './App';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import './index.scss';
 import { ConfigProvider } from 'antd';
+import App from './App';
+import 'react-datepicker/dist/react-datepicker.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
